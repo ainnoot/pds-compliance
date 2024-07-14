f44584c2fa07f97a28ce43f3965aae48b93d6472
ainnoot <ai6chr+github@gmail.com>
2024-07-14 20:42:47 +0200

    chore: example usage

---
+++ example_usage.py
@@ -0,0 +1,17 @@
+from pds_compliance import TraceFootprint, AbstractPDS
+
+if __name__ == '__main__':
+    tp = TraceFootprint({
+        1: False,
+        2: True,
+        3: False
+    })
+
+    pds = AbstractPDS({
+        1: 0.5,
+        2: 1.0,
+        3: 0.45
+    })
+
+    ans = pds.compliance(tp)
+    print(ans)
