bb7e34714debcc158dc01d7cd0230cf4ff70f63d
ainnoot <ai6chr+github@gmail.com>
2024-07-15 09:45:10 +0200

    chore: align example_usage with AbstractPDS class changes

--- example_usage.py
+++ example_usage.py
@@ -3,7 +3,7 @@ from pds_compliance import TraceFootprint, AbstractPDS
 if __name__ == "__main__":
     tp = TraceFootprint({1: False, 2: True, 3: False})
 
-    pds = AbstractPDS({1: 0.5, 2: 1.0, 3: 0.45})
+    pds = AbstractPDS.of({1: 0.5, 2: 1.0, 3: 0.45})
 
     ans = pds.compliance(tp)
     print(ans)
