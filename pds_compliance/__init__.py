b938d572c8e3cd9d3ea7891092b3007dd6588f73
ainnoot <ai6chr+github@gmail.com>
2024-07-22 14:53:44 +0200

    removing ad-hoc class for declare; just do the 2-phase approach

--- pds_compliance/__init__.py
+++ pds_compliance/__init__.py
@@ -1,3 +1,2 @@
 from pds_compliance.pds import AbstractPDS, TraceFootprint
 from pds_compliance.exceptions import MissingProbability, InvalidProbability
-from pds_compliance.declare_pds import DeclarePDS
